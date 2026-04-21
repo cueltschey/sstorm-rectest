d52499c5b4c1c026347a8f3f607cef680c4c5c8f
Charles Ueltschey <chaseuelt@gmail.com>
2026-04-21 08:26:19 -0500

    Add worker thread to main

---
+++ worker_thread.py
@@ -0,0 +1,16 @@
+from worker_thread import WorkerThread
+
+
+class sstorm(WorkerThread):
+    def start(self):
+        self.config.image_name = "ghcr.io/oran-testing/sstorm"
+        self.cleanup_old_containers()
+        self.setup_env()
+        self.setup_networks()
+        self.config.container_volumes[self.config.config_file] = {"bind": "/sstorm.conf", "mode": "ro"}
+        self.setup_volumes()
+
+        self.start_container()
+
+
+
